bd2822f5ef916513537f3cdc061dd0a8bf1fdd70
mistertandon <enggparveshtandon@gmail.com>
2022-11-01 22:18:39 +0530

    feat: basic configuration for Auth0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Auth0Provider domain={'dev-i4bzwtj2d2aulowd.us.auth0.com'} clientId={'UAe9ItrNjt3lx3xX9tXwvnsO3bLEJ3is'} redirectUri={'http://localhost:3000'}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
